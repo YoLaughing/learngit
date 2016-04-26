627222a2f73dd443faccfb088ef03f0652d5ca2f
Laughing <liufenxd@163.com>
2016-04-26 19:47:05 +0800

    add111in first

--- You.java
+++ You.java
@@ -2,7 +2,7 @@ class You
 {
 	public static void main (String[] args)
 	{
-		System.out.println("hello Java!");
+		System.out.println("hello Java!111");
 		System.out.println("hello Java222!");
 		
 	}
